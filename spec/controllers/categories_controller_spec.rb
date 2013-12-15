0dc10566674b0a3e6e717e976f14d892a7c987a3
Blaine Johnson <blainejohnson17@gmail.com>
2013-12-15 14:54:32 -0700

    Adds categories controller spec

---
+++ spec/controllers/categories_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe CategoriesController do
+
+  describe "GET #show" do
+
+    it_behaves_like "require sign in" do
+      let(:action) { get :show, id: 3 }
+    end
+
+    it "sets @category to the requested Category" do
+      set_current_user
+      commedy = Fabricate(:category)
+      get :show, id: commedy.id
+      expect(assigns(:category)).to eq(commedy)
+    end
+  end
+end
