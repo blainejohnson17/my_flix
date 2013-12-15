ea77d79da51f62e2432b9f1009c6733de6b1ff28
Blaine Johnson <blainejohnson17@gmail.com>
2013-12-15 16:11:02 -0700

    Adds feature spec for inviting a friend

--- spec/features/user_invites_a_friend_spec.rb
+++ spec/features/user_invites_a_friend_spec.rb
@@ -1,2 +1,47 @@
 require 'spec_helper'
 
+feature "User invites a friend" do
+  
+  scenario "User successfully invites a friend and friend accepts invitation" do
+    bob = Fabricate(:user)
+    sign_in(bob)
+
+    invite_a_friend
+    friend_accepts_invitation
+
+    friend_should_follow_inviter(bob)
+    inviter_should_follow_friend(bob)
+  end
+  
+  def invite_a_friend
+    visit new_invitation_path
+    fill_in "Friend's Name", with: "Alice"
+    fill_in "Friend's Email Address", with: "alice@example.com"
+    fill_in "Message", with: "Come check out Myflix!"
+    click_button "Send Invitation"
+    expect(page).to have_content("Your invitation has been sent!")
+    sign_out
+  end
+
+  def friend_accepts_invitation
+    open_email('alice@example.com')
+    current_email.click_link "Accept this invitation"
+    expect(page).to have_field('Email Address', with: 'alice@example.com')
+    fill_in "Password", with: 'password'
+    fill_in "Full name", with: 'Alice Jones'
+    click_button "Sign Up"
+    expect(page).to have_content('Welcome, Alice Jones')
+  end
+
+  def friend_should_follow_inviter(inviter)
+    click_link "People"
+    expect(page).to have_content inviter.full_name
+    sign_out
+  end
+
+  def inviter_should_follow_friend(inviter)
+    sign_in(inviter)
+    click_link "People"
+    expect(page).to have_content "Alice Jones"
+  end
+end
--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -18,6 +18,10 @@ def sign_in(user=nil)
   click_button "Sign in"
 end
 
+def sign_out
+  visit sign_out_path
+end
+
 def click_video_on_home_page(video)
   visit home_path
   find("a[href='/videos/#{video.id}']").click
