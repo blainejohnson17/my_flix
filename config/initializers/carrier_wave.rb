0fe5da3a2a273ed72380feb2ff3aaa5822c4917b
Blaine Johnson <blainejohnson17@gmail.com>
2014-03-18 01:36:14 -0600

    Revert "Moves fog credentials to fog.rb"
    This reverts commit e08bff8b0cd7c6f0ccb5d002c9a7f96340e0190d.

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,11 @@
 CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',                        # required
+      :aws_access_key_id      => 'xxx',                        # required
+      :aws_secret_access_key  => 'yyy',                        # required
+    }
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?
--- config/initializers/fog.rb
+++
@@ -1,7 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'xxx',                        # required
-    :aws_secret_access_key  => 'yyy',                        # required
-  }
-end
