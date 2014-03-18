e08bff8b0cd7c6f0ccb5d002c9a7f96340e0190d
Blaine Johnson <blainejohnson17@gmail.com>
2014-03-18 01:02:36 -0600

    Moves fog credentials to fog.rb

---
+++ config/initializers/fog.rb
@@ -0,0 +1,7 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => 'xxx',                        # required
+    :aws_secret_access_key  => 'yyy',                        # required
+  }
+end
