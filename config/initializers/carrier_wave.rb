bae8faa429c44d1e13648eaf914e2a0514e97581
Blaine Johnson <blainejohnson17@gmail.com>
2014-03-19 01:51:27 -0600

    Adds aws region to carrier_wave config and moves directory_name to env var

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,9 +5,10 @@ CarrierWave.configure do |config|
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                 => 'us-west-2',
     }
   else
-    config.fog_directory  = 'myflix.bjj'
+    config.fog_directory  = ENV['AWS_BUCKET']
     config.storage = :file
     config.enable_processing = Rails.env.development?
   end
