208b80364248a561a7a841c2fe2e9507d8559fe2
Blaine Johnson <blainejohnson17@gmail.com>
2014-03-22 21:55:35 -0600

    Modifies uploaders Adds code to create upload folder structure

--- app/uploaders/large_cover_uploader.rb
+++ app/uploaders/large_cover_uploader.rb
@@ -2,4 +2,12 @@ class LargeCoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   process :resize_to_fill => [665, 375]
+
+  def store_dir
+    if Rails.env.test?
+      "uploads/testing"
+    else
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
+  end
 end
--- app/uploaders/small_cover_uploader.rb
+++ app/uploaders/small_cover_uploader.rb
@@ -2,4 +2,12 @@ class SmallCoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   process :resize_to_fill => [166, 236]
+
+  def store_dir
+    if Rails.env.test?
+      "uploads/testing"
+    else
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
+  end
 end
