6e4b39194efacc79eb9d1744149d62e7db852535
Blaine Johnson <blainejohnson17@gmail.com>
2014-05-14 00:56:08 -0600

    Modifies image uploaders

--- app/uploaders/small_cover_uploader.rb
+++ app/uploaders/small_cover_uploader.rb
@@ -1,7 +1,7 @@
 class SmallCoverUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  process :resize_to_fill => [166, 236]
+#  process :resize_to_fill => [166, 236]
 
   def store_dir
     if Rails.env.test?
