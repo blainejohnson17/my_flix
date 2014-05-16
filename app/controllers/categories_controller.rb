4d6906a76174e8bfde5a99e72bd1f722ca4decd0
Blaine Johnson <blainejohnson17@gmail.com>
2014-05-15 23:30:44 -0600

    Adds paginate call to load @videos

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,5 +3,10 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @videos = Video.where(category_id: @category.id).paginate(:page => params[:page])
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
