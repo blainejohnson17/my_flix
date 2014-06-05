a50939cb68c99bb592841fbbcaf412bba29e7909
Blaine Johnson <blainejohnson17@gmail.com>
2014-06-05 14:30:41 -0600

    Removes old images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,58 @@
-file = File.join(Rails.root, 'db', 'tv_shows_1364.yml')
+Video.destroy_all
+Category.destroy_all
+User.destroy_all
+u = User.create(full_name: "Blaine Johnson", email: "blainejohnson17@gmail.com", admin: true)
+u.password = "password"
+u.save!
+
+file = File.join(Rails.root, 'db', 'videos_seed_test.yml')
 categorized_videos = YAML::load(File.open(file))
 
-# categorized_videos.each do |category|
-#   cat = category.find_or_create_by_name(category[:name])
-  cat = Category.find_or_create_by_name('TV Shows')
+categorized_videos.each do |video|
+  cat = Category.find_or_create_by_name(video[:category])
+  vid = Video.create(
+    title: video[:title],
+    description: video[:description],
+    remote_small_cover_url: video[:remote_small_cover_url],
+    remote_large_cover_url: video[:remote_large_cover_url],
+    category: cat,
+    year: video[:year],
+    cert_rating: video[:cert_rating],
+    duration: video[:duration]
+  )
 
-  categorized_videos.each do |video|
-    vid = Video.create(
-      title: video[:title],
-      description: video[:description],
-      remote_small_cover_url: video[:remote_small_cover_url],
-      remote_large_cover_url: video[:remote_large_cover_url],
-      category: cat
-    )
-
-    video[:reviews].each do |review|
-      user = Fabricate(:user)
-      Review.create(
-        content: review[:content],
-        user: user,
-        video: vid
-      )
-      Rating.create(
-        value: review[:rating],
-        user: user,
-        video: vid
-      )
+  if video[:creators]
+    video[:creators].each do |creator|
+      c = Artist.find_or_create_by_name(creator)
+      vid.creators << c
     end
   end
-# end
+
+  if video[:cast]
+    video[:cast].each do |actor|
+      a = Artist.find_or_create_by_name(actor)
+      vid.actors << a
+    end
+  end
+
+  if video[:reviews]
+    video[:reviews].each do |review|
+      user = Fabricate(:user)
+      if review[:content]
+        Review.create(
+          content: review[:content],
+          user: user,
+          video: vid
+        )
+      end
+
+      if review[:rating]
+        Rating.create(
+          value: review[:rating],
+          user: user,
+          video: vid
+        )
+      end
+    end
+  end
+end
