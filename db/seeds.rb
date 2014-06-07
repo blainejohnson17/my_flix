3302dcf5bb71f0d3b9e13fa879d6648cbe500dc9
Blaine Johnson <blainejohnson17@gmail.com>
2014-06-07 13:33:03 -0600

    Modifies seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
-Video.destroy_all
-Category.destroy_all
-Artist.destroy_all
-User.destroy_all
-u = User.create(full_name: "Blaine Johnson", email: "blainejohnson17@gmail.com", admin: true)
+u = User.find_or_initialize_by_email("guest@example.com")
+u.full_name = "Guest User" if u.new_record?
 u.password = "password"
 u.save!
 
-file = File.join(Rails.root, 'db', 'videos_seed_test.yml')
+file = File.join(Rails.root, 'db', 'videos_seed.yml')
 categorized_videos = YAML::load(File.open(file))
 
 categorized_videos.each do |video|
   cat = Category.find_or_create_by_name(video[:category])
-  vid = Video.create(
-    title: video[:title],
+
+  vid = Video.find_or_initialize_by_title(video[:title])
+  next unless vid.new_record?
+
+  vid.attributes = {
     description: video[:description],
     remote_small_cover_url: video[:remote_small_cover_url],
     remote_large_cover_url: video[:remote_large_cover_url],
@@ -20,7 +20,9 @@ categorized_videos.each do |video|
     year: video[:year],
     cert_rating: video[:cert_rating],
     duration: video[:duration]
-  )
+  }
+
+  next unless vid.save
 
   if video[:creators]
     video[:creators].each do |creator|
@@ -36,9 +38,12 @@ categorized_videos.each do |video|
     end
   end
 
-  if video[:reviews]
-    video[:reviews].each do |review|
-      user = Fabricate(:user)
+  reviews = video[:reviews]
+
+  if reviews
+    users = User.all.sample(reviews.count)
+    reviews.each_with_index do |review, index|
+      user = users[index]
       if review[:content]
         Review.create(
           content: review[:content],
